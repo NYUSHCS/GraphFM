65fd201e68ad989e00e97736e61f83496ba37828
Data-Centric AI and Nets Lab@NYU <qiaoyu.tan19@gmail.com>
2024-06-08 01:41:29 +0800

    Update code

--- run.sh
+++ run.sh
@@ -7,12 +7,12 @@
 
 module purge
 module load miniconda
-source activate GraphFoundationModel
+source activate GraphFM
 
 
 for dataset in 'cora' 'pubmed' 'citeseer' 'Flickr' 'Reddit' 'ogbn-arxiv'; do
    for type_model in 'GraphMAE' 'GraphMAE2' 'S2GAE' 'GraphCL' 'GraphECL' 'CCA-SSG' 'GBT' 'BGRL'; do
-      for batch_type in 'full_batch'; do
+      for batch_type in 'full_batch' 'node_sampling' 'subgraph_sampling'; do
           python -u main.py --dataset $dataset --type_model $type_model --cuda True --cuda_num 0 > ${batch_type}_${dataset}_${type_model}.txt 2>&1
       done
    done
